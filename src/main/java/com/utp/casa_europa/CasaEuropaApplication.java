89d9d78c5653f118e3b0694766aba393daca2d10
Mayki29 <zicmayki@gmail.com>
2025-05-01 11:49:20 -0500

    first commit

---
+++ src/main/java/com/utp/casa_europa/CasaEuropaApplication.java
@@ -0,0 +1,13 @@
+package com.utp.casa_europa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaEuropaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaEuropaApplication.class, args);
+	}
+
+}
