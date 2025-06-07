1a3431d2e6013254da6331d647eef91b6b53e9e0
Mayki29 <zicmayki@gmail.com>
2025-06-07 17:47:31 -0500

    add dockerfile

--- src/main/java/com/utp/casa_europa/repositories/CategoriaRepository.java
+++ src/main/java/com/utp/casa_europa/repositories/CategoriaRepository.java
@@ -1,8 +1,11 @@
 package com.utp.casa_europa.repositories;
 
-import com.utp.casa_europa.models.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.utp.casa_europa.models.Categoria;
+
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     
 }
