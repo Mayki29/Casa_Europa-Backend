80a473b0bb17d2617c173eaa2d51ce25fe2a77b3
Mayki29 <zicmayki@gmail.com>
2025-06-07 17:20:53 -0500

    add data annotation

--- src/main/java/com/utp/casa_europa/models/DetalleVenta.java
+++ src/main/java/com/utp/casa_europa/models/DetalleVenta.java
@@ -9,9 +9,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="detalle_ventas")
