0e1acb105512229a9692574b0a6907d4b80c5cfc
Mayki29 <zicmayki@gmail.com>
2025-06-16 19:38:24 -0500

    ?

--- src/main/java/com/utp/casa_europa/controllers/PaymentController.java
+++ src/main/java/com/utp/casa_europa/controllers/PaymentController.java
@@ -1,5 +1,6 @@
 package com.utp.casa_europa.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,15 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.utp.casa_europa.dtos.payment.CardPaymentDto;
+import com.utp.casa_europa.dtos.payment.ResponsePaymentDto;
+import com.utp.casa_europa.services.IPaymentService;
 
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
 
+    @Autowired
+    private IPaymentService paymentService;
+
 
     @PostMapping("/proccess_payment")
     public ResponseEntity<?> proccessPayment(@RequestBody CardPaymentDto cardPayment){
-        
-        return ResponseEntity.ok();
+        ResponsePaymentDto response = paymentService.proccessPayment(cardPayment);
+        return ResponseEntity.ok(response);
     } 
 }
